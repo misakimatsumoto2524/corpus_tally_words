caeb74a8a9bae21612dc2bb1554b2e0801f6cb52
Misaki Matsumoto <misaki.matsumoto2524@gmail.com>
2018-08-10 00:28:28 +0900

    modified the script to get the solid one but still need to fix \n

--- project2.py
+++ project2.py
@@ -1,11 +1,27 @@
 import os
 import codecs
 import re
-
 import string as s
 
 def make_set(set):
+    apo_list = {}
+    word_list = []
+    # set = filter("''", set)
+    for each in set:
+        each = re.split(' |; |,| |-|\n', each)
+        # each = list(filter(None, each))
+        print(each)
 
+    #     for i in each:
+    #         if (i in apo_list.keys()):
+    #             apo_list[i] += 1
+    #         else:
+    #             i = i.lower()
+    #             apo_list[i] = 1
+    # fix = sorted(apo_list)
+    # print(fix[0])
+    # for i in fix:
+    #     print(i + ' ' +str(apo_list[i]))
 
 
 def clean_text():
@@ -16,12 +32,11 @@ def clean_text():
         with codecs.open(filepath, "r", encoding='utf-8', errors='ignore') as f:
             content = f.read()
             tag = re.compile('<.*?>')
-            punc = re.compile("[^a-zA-Z' ]")
+            # punc = re.compile('[123456789!@#$%^&*()-=_+``/;]')
+            punc = re.compile("[^a-zA-Z(?s) ']")
             cleantext = re.sub(tag, '', content)
             cleantext = re.sub(punc, '', cleantext)
-            cleantext = cleantext.split(" ")
-    print(cleantext)
-
+            cleantext = re.findall(r"''(.*?)''", cleantext)
     make_set(cleantext)
 
 
